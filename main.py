36fbb9584cf4ea2442625e07bf71e0a0205dea8c
jblomquist2 <159498283+jblomquist2@users.noreply.github.com>
2024-12-01 15:35:46 +0000

    Updated Code

--- main.py
+++ main.py
@@ -1,23 +1,29 @@
+# Constants
 validDate = True
+MIN_DAY = 1
 MIN_YEAR = 0
 MIN_MONTH = 1
 MAX_MONTH = 12
-MIN_DAY = 1
-MAX_DAY = 31
 
-year =
-month =
-day =
+# User Inputs
+month = int(input("Enter the month: "))
+day = int(input("Enter the day: "))
+year = int(input("Enter the year: "))
+
+# If Statements
+if month in (4, 6, 9, 11):
+    maxDay = 30
+else:
+    maxDay = 31
 
 if int(year) <= MIN_YEAR: 
     validDate = False
 elif int(month) < MIN_MONTH or int(month) > MAX_MONTH: 
     validDate = False
-elif int(day) < MIN_DAY or int(day) > MAX_DAY: 
+elif int(day) < MIN_DAY or int(day) > maxDay: 
     validDate = False
 
-
-if validDate:
-    print(f"{month}/{day}/{year} is a valid date.")
+if validDate == True:
+    print(f"{month}/{day}/{year} is a valid date")
 else:
-    print(f"{month}/{day}/{year} is an invalid date. ")
+    print(f"{month}/{day}/{year} is an invalid date")
